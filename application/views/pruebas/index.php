85c79bf3ce036bc0a396d09751eea64319574f9c
JMarze <arze.jesus@gmail.com>
2017-03-23 18:53:46 -0400

    Vista - función de Controlador

---
+++ application/views/pruebas/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>Página de Prueba</title>
+		<meta charset="utf-8"/>
+	</head>
+
+	<body>
+		<h1>Hola mi nombre es: <i>CodeIgniter</i></h1>
+	</body>
+</html>
